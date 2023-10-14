91f603205cc5e81049980d1c791c5a76fba3063d
Josh <joshscoper@gmail.com>
2023-10-14 16:58:42 -0600

    Updated Restricted Movements (Water+Lava)

--- src/main/java/net/fabledrealms/listeners/PlayerMoveListener.java
+++ src/main/java/net/fabledrealms/listeners/PlayerMoveListener.java
@@ -1,6 +1,7 @@
 package net.fabledrealms.listeners;
 
 import net.fabledrealms.Core;
+import net.fabledrealms.animations.RespawnAnimation;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -19,20 +20,45 @@ public class PlayerMoveListener implements Listener {
         Bukkit.getPluginManager().registerEvents(this, main);
     }
 
-    private void playerAntiSwim(Player player){
+    private void playerAntiSwim(Player player, Location toLoc){
+        //Finds the closest safe block within radius, if none then teleports player to the nearest graveyard
         String message = main.getLangFile().getFile().getString("player.enter-water");
         player.sendMessage(main.getStringUtil().colorString(message));
         player.playSound(player.getLocation(), Sound.ENTITY_VILLAGER_NO,1,1);
+        int radius = 10;
+        for (int x = -(radius); x <= radius; x ++) {
+            for (int y = -(radius); y <= radius; y ++)
+            {
+                for (int z = -(radius); z <= radius; z ++)
+                {
+                    if (!player.getWorld().getBlockAt(x,y,z).getType().equals(Material.WATER) && !player.getWorld().getBlockAt(x,y,z).equals(Material.AIR)
+                    && !player.getWorld().getBlockAt(x,y,z).getType().equals(Material.LAVA)){
+                        player.teleport(player.getWorld().getBlockAt(x,y,z).getLocation());
+                        return;
+                    }
+                    player.teleport(main.getGraveyardManager().getClosestGraveyard(player));
+                }
+            }
+        }
     }
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent event){
         Player player = event.getPlayer();
         Location toLoc = event.getTo();
         Location fromLoc = event.getFrom();
-        if (toLoc.getBlock().getType().equals(Material.WATER)){
-            event.setCancelled(true);
-            playerAntiSwim(player);
-            return;}
+        if (!player.hasPermission("fr.bypass.restrictedareas")) {
+            if (toLoc.getBlock().getType().equals(Material.WATER)) {
+                event.setCancelled(true);
+                playerAntiSwim(player, toLoc);
+                return;
+            }
+            if (toLoc.getBlock().getType().equals(Material.LAVA)){
+                event.setCancelled(true);
+                player.teleport(main.getGraveyardManager().getClosestGraveyard(player));
+                new RespawnAnimation(main, player);
+                return;
+            }
+        }
     }
 
 }
