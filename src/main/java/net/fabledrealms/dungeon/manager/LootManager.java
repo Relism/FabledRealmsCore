f8e274b43db05473824c7b5c429ed627b7389694
KacpersDev <vkacperpl@gmail.com>
2023-10-21 12:43:22 +0200

    load method

--- src/main/java/net/fabledrealms/dungeon/manager/LootManager.java
+++ src/main/java/net/fabledrealms/dungeon/manager/LootManager.java
@@ -5,6 +5,7 @@ import net.fabledrealms.Core;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 public class LootManager {
 
@@ -16,13 +17,25 @@ public class LootManager {
     }
 
     public void load(){
-
+        if (this.main.getDungeonFileWrapper().getFile().getConfigurationSection("floor") == null) return;
+        for (String floor : Objects.requireNonNull(this.main.getDungeonFileWrapper().getFile().getConfigurationSection("floor")).getKeys(false)) {
+            if (this.main.getDungeonFileWrapper().getFile().getConfigurationSection("floor." + floor + ".chance") == null) return;
+            for (String chance : Objects.requireNonNull(this.main.getDungeonFileWrapper().getFile().getConfigurationSection("floor." + floor + ".chance")).getKeys(false)) {
+                Map<Integer, List<String>> entries = new HashMap<>();
+                entries.put(this.main.getDungeonFileWrapper().getFile().getInt("floor." + floor + ".chance-amount"),
+                        this.main.getDungeonFileWrapper().getFile().getStringList("floor." + floor + ".chance." + chance + ".list"));
+                map.put(this.main.getDungeonFileWrapper().getFile().getInt("floor." + floor + ".floor"), entries);
+            }
+        }
     }
 
     public void save(){
         for (Map.Entry<Integer, Map<Integer, List<String>>> entry : map.entrySet()) {
             this.main.getDungeonFileWrapper().getFile().set("floor." + entry.getKey() + ".floor", entry.getKey());
-            entry.getValue().forEach((chance, list) -> this.main.getDungeonFileWrapper().getFile().set("floor." + entry.getKey() + ".chance." + chance + ".list", list));
+            entry.getValue().forEach((chance, list) -> {
+                this.main.getDungeonFileWrapper().getFile().set("floor." + entry.getKey() + ".chance-amount", chance);
+                this.main.getDungeonFileWrapper().getFile().set("floor." + entry.getKey() + ".chance." + chance + ".list", list)
+            });
             this.main.getDungeonFileWrapper().saveFile();
         }
     }
